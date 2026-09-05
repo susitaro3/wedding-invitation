d32fe1692c4f2a81ff20d3aa2ac2de10506d92c5
susitaro3 <70739647+susitaro3@users.noreply.github.com>
2026-09-05 20:40:22 +0900

    Update WEDDING_API_URL to production URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script をウェブアプリとしてデプロイした後、/exec URL を入力してください。
-window.WEDDING_API_URL = 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE';
+window.WEDDING_API_URL = 'https://script.google.com/macros/s/AKfycbwHnELQx__EPdOyXjZFfMbUu8po3mTVUO2_rbXWkVDE4MJ7MYGEOuCgK0l4mCsDgk6oyw/exec';
