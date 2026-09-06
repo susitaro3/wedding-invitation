81b97e7acfac7f69cc7aa702b36b681116e7ed9f
susitaro3 <70739647+susitaro3@users.noreply.github.com>
2026-09-06 20:40:20 +0900

    Update WEDDING_API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script をウェブアプリとしてデプロイした後、/exec URL を入力してください。
-window.WEDDING_API_URL = 'https://script.google.com/macros/s/AKfycbwyWLx_AqZxKczZ3UlXQbJvYMzSjgwRF3r0OHqNqhz3hhherfrK5MxQFCQp9Xo3V6j1/exec';
+window.WEDDING_API_URL = 'https://script.google.com/macros/s/AKfycbyB_-i4x0q4-XxKHTxtLmhx921t_X9gWWGfPXobf7QeqODG3n0h1BKeBk452WWGSBBtJQ/exec';
