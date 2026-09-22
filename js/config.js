7ef4278fd8cc2189e96009b75dcf164ac4ed7818
susitaro3 <70739647+susitaro3@users.noreply.github.com>
2026-09-23 00:43:46 +0900

    Update GAS_WEB_APP_URL in config.js
    Updated GAS_WEB_APP_URL to the new deployment URL.

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
   // Google Apps Script でデプロイした Web App の URL をここに貼り付けてください
-  GAS_WEB_APP_URL: "YOUR_GAS_WEB_APP_URL_HERE",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyB_-i4x0q4-XxKHTxtLmhx921t_X9gWWGfPXobf7QeqODG3n0h1BKeBk452WWGSBBtJQ/exec",
 
   // 式の基本情報（画面ヘッダー等で表示）
   GROOM_NAME: "新郎 太郎",
